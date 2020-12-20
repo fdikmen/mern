d2d63f9aa5509d3fa1b1ddf3f41ff0ce0a7b8fed
frtdkmn <frtdkmn@gmail.com>
2020-12-20 19:15:10 +0300

    Add filter func to phoneBook App.

--- Lesson-5(PhoneBook App)/phonebook/src/Components/List.js
+++ Lesson-5(PhoneBook App)/phonebook/src/Components/List.js
@@ -6,13 +6,32 @@ export default class List extends Component {
   static propTypes = {
     contacts: PropsTypes.array.isRequired,
   };
+  
+  state={filterText:''};
+  filterListFunc =(e)=>
+  {
+    console.log("Run Filter...");
+    this.setState({filterText:e.target.value});
+    console.log(this.state.filterText);
+  }
+
   render() {
+
+const filteredDatas = this.props.contacts.filter((item) => {
+  return (
+    item.text.toLowerCase().indexOf(this.state.filterText.toLowerCase()) !== -1
+  );
+});
+console.log(this.props.contacts,"<=props --------------");
+console.log(filteredDatas,"<=filteredData******************");
+
     return (
       <div className="listField">
-        <input          name={"filter"}          id={"filter"}          placeholder={"Filter by name or phone ..."}
+        <input    onChange={this.filterListFunc}      name={"filter"}          id={"filter"}          placeholder={"Filter by name or phone ..."}
         ></input>
+        
         <ul>
-          {this.props.contacts.map((item,index) => {
+          {filteredDatas.map((item,index) => {
             return(<li key={index+1}>
                 <span className={"name"}>{item.text}</span>
                 <span className={"phone"}>{item.phone}</span>
---
+++ Lesson-6(React LifeCycle)/lifecycle/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Main from './Main';
+
+function App() {
+  return (
+    <div className="App">
+     <Main testProps="123"></Main>
+    </div>
+  );
+}
+
+export default App;
