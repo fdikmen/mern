e9fc23768505393f570c0b0bc141d68f2785f267
frtdkmn <frtdkmn@gmail.com>
2021-01-08 14:16:35 +0300

    Fetching & Web API Call

---
+++ Lesson-07(Fetching)/fetching/src/Error.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Error = () => {
+    return (
+        <h1>
+          Error Page <br /> Upss :-(<br/>
+            Page Not Found!
+        </h1>
+      );
+};
+
+export default Error;
