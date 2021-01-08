2644f346bb897a2017f0caced8ae45fc29e98455
frtdkmn <frtdkmn@gmail.com>
2021-01-08 14:18:43 +0300

    EcmaScript Module System

--- Lesson-02(EcmaScript_ModuleSystem)/ecmamodule/index.js
+++ Lesson-02(EcmaScript_ModuleSystem)/ecmamodule/index.js
@@ -1,7 +1,7 @@
 //import multiply,{sumNum as topla,divNum} from './mathModule'; //=>We can use the name we want with "AS" operator.
 import * as mathMod from "./mathModule";
 
-console.log("Hello!");
+console.log("Hello!!!");
 
 // console.log(sumNum(2, 5));   //=>Without "* as".
 console.log(mathMod.sumNum(2, 5));
