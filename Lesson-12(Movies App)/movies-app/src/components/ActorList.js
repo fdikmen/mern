73f95fef02ccd005f2508f0e06dca8c89f7c92ec
frtdkmn <frtdkmn@gmail.com>
2021-01-21 20:50:50 +0300

    Published.

--- Lesson-12(Movies App)/movies-app/src/components/ActorList.js
+++ Lesson-12(Movies App)/movies-app/src/components/ActorList.js
@@ -1,39 +1,23 @@
 import React from "react";
-import PropTypes from "prop-types";
-import { Card, Image,Grid } from "semantic-ui-react";
+import { Card, Image, Grid } from "semantic-ui-react";
 
-function ActorList(props) {
+export default function ActorList(props) {
   return (
     <div>
-
-<Grid columns={2}>
-    <Grid.Row>
-      {props.actorList.map((actor,index)=>(
-        <Grid.Column key={index}>
-        <Card>
-        <Image src={actor.photoUrl} wrapped ui={false} />
-        <Card.Content>
-          <Card.Header>{actor.name}</Card.Header>
-          {/* <Card.Description>
-            {actor.desc}
-          </Card.Description> */}
-        </Card.Content>
-      </Card>
-      </Grid.Column>
-      ))}
-    </Grid.Row>
-
-  </Grid>
-
-
-
-
-      
-      
+      <Grid columns={2}>
+        <Grid.Row>
+          {props.actorList.map((actor, index) => (
+            <Grid.Column key={index}>
+              <Card>
+                <Image src={actor.photoUrl} wrapped ui={false} />
+                <Card.Content>
+                  <Card.Header>{actor.name}</Card.Header>
+                </Card.Content>
+              </Card>
+            </Grid.Column>
+          ))}
+        </Grid.Row>
+      </Grid>
     </div>
   );
 }
-
-ActorList.propTypes = {};
-
-export default ActorList;
